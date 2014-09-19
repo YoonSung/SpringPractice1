10a55fd83568b1651ed5bed5a13e6a76512bf05f
Yoonsung <lvev9925@naver.com>
2014-09-19 21:46:30 +0900

    Add UserController for register Page View

---
+++ FirstPractice/src/main/java/com/practice/spring/UserController.java
@@ -0,0 +1,13 @@
+package com.practice.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping("/users/form")
+	public String form() {
+		return "/users/form";
+	}
+}
