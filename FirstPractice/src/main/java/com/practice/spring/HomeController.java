81ab282d9aa2720aff853564a5399427c1e47bfa
Yoonsung <lvev9925@naver.com>
2014-09-19 21:38:46 +0900

    Rename index to home, Add Frontend Files from slipp project

--- FirstPractice/src/main/java/com/practice/spring/HomeController.java
+++ FirstPractice/src/main/java/com/practice/spring/HomeController.java
@@ -7,14 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
-	
-	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
-	
+public class HomeController {
+
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
 	@RequestMapping("/")
 	public String entrance(Model model) {
 		log.info("Logger Test!");
 		model.addAttribute("test", System.currentTimeMillis());
-		return "index";
+		return "home";
 	}
 }
