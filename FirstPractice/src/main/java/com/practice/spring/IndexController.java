162daa48c29487914c3861e0e81b292f0369f217
Yoonsung <lvev9925@naver.com>
2014-09-19 17:56:17 +0900

    Logging Framework (Logback) 적용

--- FirstPractice/src/main/java/com/practice/spring/IndexController.java
+++ FirstPractice/src/main/java/com/practice/spring/IndexController.java
@@ -1,5 +1,7 @@
 package com.practice.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+	
 	@RequestMapping("/")
 	public String entrance(Model model) {
-		System.out.println("request!");
+		log.info("Logger Test!");
 		model.addAttribute("test", System.currentTimeMillis());
 		return "index";
 	}
