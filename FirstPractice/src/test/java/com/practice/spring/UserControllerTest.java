f4a60c1b4d3fba2f0d803626b6642b6bdfc8c49a
Yoonsung <lvev9925@naver.com>
2014-09-20 15:32:37 +0900

    Request Register User Data Test Complete.

--- FirstPractice/src/test/java/com/practice/spring/UserControllerTest.java
+++ FirstPractice/src/test/java/com/practice/spring/UserControllerTest.java
@@ -1,20 +1,35 @@
 package com.practice.spring;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
 
 public class UserControllerTest {
 
+	MockMvc mockMvc;
+	
+	@Before
+	public void setUp() {
+		mockMvc = standaloneSetup(new UserController()).build(); 
+	}
+	
 	@Test
 	public void forward() throws Exception {
-		standaloneSetup(new UserController())
-		.build()
-			.perform(get("/user/form"))
+		mockMvc.perform(get("/user/form"))
 			.andExpect(status().isOk())
 			.andExpect(forwardedUrl("/users/form"));
+	}
+	
+	@Test
+	public void create() throws Exception {
+		mockMvc.perform(post("/user"))
+			.andExpect(status().isFound())
+			.andExpect(redirectedUrl("/"));
 	}
 }
