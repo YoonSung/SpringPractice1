67371dce6d68546a340ac8dff21e39ccb9ec9ecd
Yoonsung <lvev9925@naver.com>
2014-09-21 14:54:16 +0900

    Refactoring pom.xml (properties for spring version)

--- FirstPractice/src/main/java/com/practice/spring/HomeController.java
+++ FirstPractice/src/main/java/com/practice/spring/HomeController.java
@@ -1,7 +1,5 @@
 package com.practice.spring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
