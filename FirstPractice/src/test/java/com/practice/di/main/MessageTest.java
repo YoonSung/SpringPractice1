8c7179864b37cedd670b73d99dc51dc069e7068d
Yoonsung <lvev9925@naver.com>
2014-09-21 11:44:38 +0900

    di from Spring Configuration File (di.xml)

--- FirstPractice/src/test/java/com/practice/di/main/MessageTest.java
+++ FirstPractice/src/test/java/com/practice/di/main/MessageTest.java
@@ -1,5 +1,7 @@
 package com.practice.di.main;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.practice.di.provider.HiWorldMessageProvider;
 import com.practice.di.provider.MessageProvider;
 import com.practice.di.renderer.DefaultMessageRenderer;
@@ -8,6 +10,19 @@ import com.practice.di.renderer.MessageRenderer;
 public class MessageTest {
 
 	public static void main(String[] args) {
+		//codeLevelDI();
+		springXMLLevelDI();
+	}
+
+	public static void springXMLLevelDI() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/di.xml");
+		MessageRenderer messageRenderer = context.getBean("messageRenderer", MessageRenderer.class);
+		messageRenderer.render();
+		
+		context.close();
+	}
+
+	public static void codeLevelDI() {
 		MessageProvider messageProvider = new HiWorldMessageProvider();
 
 		MessageRenderer messageRenderer = new DefaultMessageRenderer();
