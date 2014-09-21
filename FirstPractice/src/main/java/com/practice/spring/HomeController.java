df6a10be7d1c90698b0b2f6d4b54f478667241d8
Yoonsung <lvev9925@naver.com>
2014-09-21 14:49:41 +0900

    1. Set UserController Dependency Injection UserDao By Spring Container. 2. Home Controller's Test Code Delete

--- FirstPractice/src/main/java/com/practice/spring/HomeController.java
+++ FirstPractice/src/main/java/com/practice/spring/HomeController.java
@@ -3,18 +3,13 @@ package com.practice.spring;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
-
 	@RequestMapping("/")
-	public String entrance(Model model) {
-		log.info("Logger Test!");
-		model.addAttribute("test", System.currentTimeMillis());
+	public String entrance() {
 		return "home";
 	}
 }
--- FirstPractice/src/main/java/com/practice/spring/UserController.java
+++ FirstPractice/src/main/java/com/practice/spring/UserController.java
@@ -2,6 +2,7 @@ package com.practice.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,9 @@ public class UserController {
 	
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
+	@Autowired
+	UserDao userDao;
+	
 	@RequestMapping("/user/form")
 	public String form() {
 		return "/users/form";
@@ -19,6 +23,11 @@ public class UserController {
 	@RequestMapping(value="/user", method=RequestMethod.POST)
 	public String create(User user) {
 		log.info("Create User : {}", user);
+		
+		//TODO Validation Check
+		int affectedRow = userDao.create(user);
+		
+		//TODO create Result Check, 
 		
 		return "redirect:/";
 	}
