fd747a4ac0387a678c38f545d87b3e78454ee136
Yoonsung <lvev9925@naver.com>
2014-09-23 15:45:40 +0900

    Add flyway db migration

--- FirstPractice/src/main/java/com/practice/dao/MybatisUserDao.java
+++ FirstPractice/src/main/java/com/practice/dao/MybatisUserDao.java
@@ -1,14 +1,8 @@
 package com.practice.dao;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import com.practice.domain.User;
 
@@ -16,31 +10,11 @@ public class MybatisUserDao implements UserDao{
 
 	private static final Logger log = LoggerFactory.getLogger(MybatisUserDao.class);
 	
-	//TODO DELETE
 	//TODO SET AUTOWIRED
-	private DataSource dataSource;
 	private SqlSession sqlSession;
-	
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
 	
 	public void setSqlSession(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
-	}
-	
-	public DataSource getDataSource() {
-		return dataSource;
-	}
-	
-	@PostConstruct
-	public void init() {
-		
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("user.sql"));
-		DatabasePopulatorUtils.execute(populator, getDataSource());
-		
-		log.info("Database Initialize Success!!");
 	}
 	
 	@Override
